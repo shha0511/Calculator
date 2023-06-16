62033c3bfe9250dec18f89503e676a0cec7852d9
USER_A <USER_A@aaa.co.kr>
2023-06-16 14:29:40 +0900

    Release v2.0.1

--- ctrl.py
+++ ctrl.py
@@ -25,18 +25,18 @@ class Control:
         self.view.btn1.clicked.connect(lambda: self.view.setDisplay(self.calculate()))
         self.view.btn2.clicked.connect(self.view.clearMessage)
         
-    def sum(self, a,b):
+    def sum(self, a, b):
         return a+b
         
-    def sub(self, a,b):
+    def sub(self, a, b):
         return a-b
     
-    def mul(self, a,b):
+    def mul(self, a, b):
         return a*b
     
-    def div(self, a,b):
+    def div(self, a, b):
         return a/b
     
-    def pow(self, a,b):
+    def pow(self, a, b):
         return pow(a, b)
     
