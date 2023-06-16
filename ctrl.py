7ea2ce7924e9d1a72190db366e60007cf4e5ec2a
USER_A <USER_A@aaa.co.kr>
2023-06-16 14:12:09 +0900

    aa

--- ctrl.py
+++ ctrl.py
@@ -6,11 +6,18 @@ class Control:
     def calculate(self):
         num1 = float(self.view.le1.text())
         num2 = float(self.view.le2.text())
-        operator =self.view.cb.currentText()
+        operator = self.view.cb.currentText()
         
-        if operator =='+':
+        if operator == '+':
             return f'{num1} + {num2} = {self.sum(num1, num2)}'
-        
+        elif operator == '-':
+            return f'{num1} - {num2} = {self.sub(num1, num2)}'
+        elif operator == '*':
+            return f'{num1} * {num2} = {self.mul(num1, num2)}'
+        elif operator == '/':
+            return f'{num1} / {num2} = {self.div(num1, num2)}'
+        elif operator == '^':
+            return f'{num1} ^ {num2} = {self.pow(num1, num2)}'
         else:
             return "Calculation Error"
         
@@ -19,5 +26,17 @@ class Control:
         self.view.btn2.clicked.connect(self.view.clearMessage)
         
     def sum(self, a, b):
-            return a+b
+        return a+b
         
+    def sub(self, a, b):
+        return a-b
+    
+    def mul(self, a, b):
+        return a*b
+    
+    def div(self, a, b):
+        return a/b
+    
+    def pow(self, a, b):
+        return pow(a, b)
+    
